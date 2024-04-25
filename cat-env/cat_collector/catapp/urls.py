c3b189e12888cc1e4da86a9994e5ed05bfd640fe
zahra <alzahra@live.com>
2024-04-25 06:33:52 +0300

    curd

--- cat-env/cat_collector/catapp/urls.py
+++ cat-env/cat_collector/catapp/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.catapp, name='catapp'),
     path('review/', views.review, name='review'),
+    path('addCats/', views.addCat, name='addCat'),
+    path('updateCat/<int:bId>', views.updateCat, name='updateCat'),
+    path('filterCats', views.filterCats, name="filterCats")
 ]
--- cat-env/cat_collector/catapp/views.py
+++ cat-env/cat_collector/catapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from .models import Cat
@@ -13,6 +13,58 @@ def review(request):
     return HttpResponse(template.render())
 
 
+def addCat(request):
+    if request.method == 'POST':
+        nameval = request.POST.get('name')
+        imageval = request.FILES.get('image')
+        rarityval = request.POST.get('rarity')
+        descriptionval = request.POST.get('description')
+        obj = Cat(name=nameval, image=imageval, rarity=rarityval, description=descriptionval)
+        obj.save()
+        return redirect('cat', cId=obj.id)
+    return render(request, "addCat.html", {})
+
+def updateCat(request, cId):
+    obj = Cat.objects.get(id=cId)
+    if request.method == 'POST':
+        nameval = request.POST.get('name')
+        imageval = request.FILES.get('image')
+        rarityval = request.POST.get('rarity')
+        descriptionval = request.POST.get('description')
+        obj.name = nameval
+        obj.image = imageval
+        obj.rarity = rarityval
+        obj.description = descriptionval
+        obj.save()
+        return redirect('cat', cId=obj.id)
+    return render(request, "updateCat.html", {'obj': obj})
+
+
+def filterCats(request):
+    if request.method == "POST":
+        string = request.POST.get('keyword').lower()
+        isName = request.POST.get('option1')
+        isRarity = request.POST.get('option2')
+
+        selected = request.POST.get('selectedRarity')
+
+        myCats = Cat.objects.filter(name__icontains='or')
+        myCats2 = myCats.filter(rarity__lte=100).exclude(description__icontains='Saad')
+
+        print(f"selected rarity = {selected}")
+        # now filter
+        cats = __getCats()
+        newCats = []
+        for item in cats:
+            contained = False
+            if isName and string in item['name'].lower():
+                contained = True
+            if not contained and isRarity and string in item['rarity'].lower():
+                contained = True
+            if contained:
+                newCats.append(item)
+        return render(request, 'catList.html', {'cats': newCats})
+    return render(request, 'search.html', {})
 
 
 
